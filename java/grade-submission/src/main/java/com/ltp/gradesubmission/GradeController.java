f89113372cce33bf46de805c3b0f6206ad64a498
danielderre <danielderre@outlook.com>
2023-01-20 16:18:09 -0300

    add form post method to grade submission project

--- java/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ java/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -3,17 +3,31 @@ package com.ltp.gradesubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class GradeController {
 
-    @GetMapping("/grades")
-    public String getGrades(Model model ) {
+    List<Grade> studentGrades = new ArrayList<>();
 
-        Grade grade = new Grade("Barry","Chemistry","B+");
-        model.addAttribute("grade", grade);
-
-        return "grades";
+    @GetMapping("/")
+    public String getForm(Model model) {
+        model.addAttribute("grade",new Grade());
+        return "form";
     }
 
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade) {
+        studentGrades.add(grade);
+        return "redirect:/grades";
+    }
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
 }
